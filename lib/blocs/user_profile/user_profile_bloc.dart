93433f72f37fbe87d2fd7541f36244358a2a7db2
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-05-03 04:00:11 +0500

    AUTH

--- lib/blocs/user_profile/user_profile_bloc.dart
+++ lib/blocs/user_profile/user_profile_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nur_pay/blocs/user_profile_bloc/user_profile_event.dart';
-import 'package:nur_pay/blocs/user_profile_bloc/user_profile_state.dart';
+import 'package:nur_pay/blocs/user_profile/user_profile_event.dart';
+import 'package:nur_pay/blocs/user_profile/user_profile_state.dart';
 
 class UserProfileBloc extends Bloc<UserProfileEvent, UserProfileState> {
   UserProfileBloc() : super(Initial()) {
