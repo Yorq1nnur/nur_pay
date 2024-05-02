1133e95e6cca2b3262813fc051ba763909f6f33e
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-05-03 04:24:52 +0500

    AUTH

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -116,7 +116,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
             phoneNumber: userCredential.user!.phoneNumber ?? '',
             email: userCredential.user!.email ?? '',
             fcmToken: '',
-            authUUId: '',
+            authUUId: userCredential.user!.uid,
           ),
         ),
       );
@@ -130,30 +130,3 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     }
   }
 }
-
-// Future<void> signInWithGoogle(BuildContext context,
-//     [String? clientId]) async {
-//   // Trigger the authentication flow
-//   _notify(true);
-//
-//   final GoogleSignInAccount? googleUser =
-//   await GoogleSignIn(clientId: clientId).signIn();
-//
-//   // Obtain the auth details from the request
-//   final GoogleSignInAuthentication? googleAuth =
-//   await googleUser?.authentication;
-//
-//   // Create a new credential
-//   final credential = GoogleAuthProvider.credential(
-//     accessToken: googleAuth?.accessToken,
-//     idToken: googleAuth?.idToken,
-//   );
-//
-//   // Once signed in, return the UserCredential
-//   UserCredential userCredential =
-//   await FirebaseAuth.instance.signInWithCredential(credential);
-//   _notify(false);
-//   if (userCredential.user != null) {
-//     Navigator.pushReplacementNamed(context, RouteNames.tabRoute);
-//   }
-// }
