f5fd2b5408605e887b68bf6638f2aaa4a65d70a6
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-05-07 01:49:18 +0500

    LOCAL AUTH

--- lib/screens/tab/card/card_screen.dart
+++ lib/screens/tab/card/card_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_utils/my_utils.dart';
+import 'package:nur_pay/utils/colors/app_colors.dart';
+import 'package:nur_pay/utils/sizedbox/get_sizedbox.dart';
 import 'package:nur_pay/utils/utility_functions/utility_functions.dart';
 
 class CardScreen extends StatefulWidget {
@@ -19,9 +22,36 @@ class _CardScreenState extends State<CardScreen> {
       value: systemUiOverlayStyle,
       child: Scaffold(
         appBar: AppBar(
+          actions: [
+            Ink(
+              height: 30.w,
+              width: 30.w,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  50,
+                ),
+                color: Colors.blueGrey.withOpacity(
+                  0.2,
+                ),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(
+                  50,
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    color: AppColors.black,
+                    size: 20.w,
+                  ),
+                ),
+              ),
+            ),
+            10.getW(),
+          ],
           centerTitle: true,
           title: const Text(
-            "Cards Screen",
+            "My cards",
           ),
         ),
       ),
