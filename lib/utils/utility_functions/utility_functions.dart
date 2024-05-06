7dd7fa268636f1932aee022f29b2978c66be4bf6
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-05-07 01:43:56 +0500

    LOCAL AUTH

--- lib/utils/utility_functions/utility_functions.dart
+++ lib/utils/utility_functions/utility_functions.dart
@@ -11,7 +11,7 @@ SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
 );
 
 void methodPrint(dynamic data) {
-  methodPrint("\$\$\$\$\$\$\n$data\n\$\$\$\$\$\$");
+  debugPrint("\$\$\$\$\$\$\n$data\n\$\$\$\$\$\$");
 }
 
 showSnackBar({
